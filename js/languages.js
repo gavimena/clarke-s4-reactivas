e11edde786c00a4bee6e2d37f3daf707466281d7
Gabi Mena <gabriela.grojas@gmail.com>
2018-01-20 21:16:56 +0100

    add interests and languages js files

---
+++ js/languages.js
@@ -0,0 +1,32 @@
+
+var languageName = document.querySelector(".languages");
+var languageLevel = document.querySelector(".level");
+var languageListPreview = document.querySelector(".preview__data__languages");
+var languageList = [];
+
+
+function addLanguage() {
+	var language = {
+		name: languageName.value,
+		lvl:languageLevel.value
+	};
+	languageList.push(language);
+	var allLanguageList = '';
+
+	for (var i = 0; i < languageList.length; i++) {
+		allLanguageList += '<li>' + languageList[i].name +'</li>';
+		allLanguageList += '<li>' + languageList[i].lvl +'</li><hr class="line">';
+
+	}
+	languageListPreview.innerHTML = allLanguageList;
+	document.querySelector(".languages").value = '';
+	document.querySelector('.level').value = '';
+	vistaPrevia("preview");
+}
+var botonIdioma = document.querySelector('.prueba_language');
+botonIdioma.addEventListener('click', addLanguage);
+
+function deleteLanguage() {
+	languageList = [];
+	languageListPreview.innerHTML = '';
+}
