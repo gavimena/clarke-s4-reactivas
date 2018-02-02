69dc33cc2c0d62ba91012079c8f6b6819960fc6b
gala1234 <calero.vg@gmail.com>
2018-02-02 11:22:54 +0100

    import logo on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../images/Mediaqueers.png';
 
 class Header extends React.Component {
 
@@ -7,11 +8,10 @@ class Header extends React.Component {
 	render() {
 		return(
 			<header className="header">
-				<a href="#home"><img src="../images/Mediaqueers.png" className="logo--header" alt="logo-header__menu" /></a>
+				<a href="#home"><img src={logo} className="logo--header" alt="logo-header__menu" /></a>
 				<nav className="header__menu list">
 					<li className="header__menu__item header__menu__section"><a className="header__menu__section--link" href="#home">Home</a></li>
 					<li className="header__menu__item header__menu__item--vista header__menu__section" onClick={this.props.clickPreview}><a className="header__menu__section--link" href="#vista">Vista previa</a></li>
-					<li className="header__menu__item__print"  onclick="printCurriculum()" id="btnprint" title="Imprimir"></li>
 				</nav>
 			</header>
 		);
