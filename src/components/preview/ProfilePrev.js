05f1cde0ffc7cc152c9e6019e5f2e1573c2589ba
gala1234 <calero.vg@gmail.com>
2018-02-05 09:19:10 +0100

    add profile photo on cv

--- src/components/preview/ProfilePrev.js
+++ src/components/preview/ProfilePrev.js
@@ -4,10 +4,10 @@ class Profile extends React.Component {
 	render(){
 		return(
 			<div className="printPreview preview__container--print preview__container--padding" id="preview2">
+				<div className="preview__photo" id="preview_photo"></div>
 				<div className="resume-header preview__container--padding">
 					<h2 className="resume__title" id="data-profile">{this.props.name}   {this.props.lastname}</h2>
 				</div>
-
 				<div className="resume__header__profession preview__container--padding">
 					<span id="data-profession">{this.props.profession}</span>
 				</div>
