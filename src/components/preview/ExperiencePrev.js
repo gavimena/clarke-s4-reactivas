ca9a2d064f6661e18335b9fe85b72c6d4fa9eaae
Gabi Mena <gabriela.grojas@gmail.com>
2018-02-05 14:16:16 +0100

    add months to preview with styles

--- src/components/preview/ExperiencePrev.js
+++ src/components/preview/ExperiencePrev.js
@@ -12,8 +12,8 @@ class Experience extends React.Component {
 					<ul id="data-position" className="preview__experience--position">
 						<li>{this.props.position}</li>
 						<li>{this.props.company}</li>
-						<li>{this.props.monthStart} {this.props.yearStart}</li>
-						<li>{this.props.monthEnd} {this.props.yearEnd}</li>
+						<li>{this.props.monthStart} {`-`} {this.props.yearStart}</li>
+						<li>{this.props.monthEnd} {`-`} {this.props.yearEnd}</li>
 					</ul>
 				</div>
 			</div>
